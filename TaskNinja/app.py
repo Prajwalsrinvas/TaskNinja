4be779539286c07bc40a93c3d085bd60b36bfa74
Prajwalsrinvas <24704548+Prajwalsrinvas@users.noreply.github.com>
2020-09-11 21:29:46 +0530

    Add files via upload

---
+++ TaskNinja/app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, url_for, request, redirect, send_file
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import csv
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Task {self.id}"
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an issue deleting that task'
+
+
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating that task'
+
+    else:
+        return render_template('update.html', task=task)
+
+
+@app.route("/export")
+def export():
+    outfile = open('todo.csv', 'a')
+    outcsv = csv.writer(outfile)
+    records = db.session.query(Todo).all()
+    outcsv.writerow([column.name for column in Todo.__mapper__.columns])
+    [outcsv.writerow([getattr(curr, column.name)
+                      for column in Todo.__mapper__.columns]) for curr in records]
+    outfile.close()
+    return send_file('todo.csv', as_attachment=True)
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
